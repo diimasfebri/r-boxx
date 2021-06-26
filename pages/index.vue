d164342e80f2a3408f220b4fbb5b8695a6ce614a
Aditya930GO <60997538+Aditya930GO@users.noreply.github.com>
2021-06-26 15:51:50 +0700

    Adit

--- pages/index.vue
+++ pages/index.vue
@@ -38,6 +38,88 @@
       </div>
     </div>
     <new-member v-if="tambahMember" @tutup-popup="tambahMember = false" />
+    <div v-if="type === 1" class="table-header">
+      <div class="head number">
+        <p>NIK</p>
+      </div>
+      <div class="head name">
+        <p>Nama</p>
+      </div>
+      <div class="head name">
+        <p>rewards</p>
+      </div>
+      <div class="head name">
+        <p>transaction</p>
+      </div>
+      <div class="head name">
+        <p>Nama</p>
+      </div>
+    </div>
+    <div v-else-if="type === 2" class="table-header">
+      <div
+        class="head record"
+        :class="sort.name === 'record' ? 'active' : ''"
+        @click="changeSort('record')"
+      >
+        <span class="arrow" :class="sort.type">
+          <v-icon class="icon">mdi-chevron-down</v-icon>
+        </span>
+        <p>No</p>
+      </div>
+      <div
+        class="head entry-date"
+        :class="sort.name === 'entry-date' ? 'active' : ''"
+        @click="changeSort('entry-date')"
+      >
+        <span class="arrow" :class="sort.type">
+          <v-icon class="icon">mdi-chevron-down</v-icon>
+        </span>
+        <p>Tanggal Timbang</p>
+      </div>
+      <div
+        class="head material"
+        :class="sort.name === 'material' ? 'active' : ''"
+        @click="changeSort('material')"
+      >
+        <span class="arrow" :class="sort.type">
+          <v-icon class="icon">mdi-chevron-down</v-icon>
+        </span>
+        <p>Material</p>
+      </div>
+      <div
+        class="head customer"
+        :class="sort.name === 'customer' ? 'active' : ''"
+        @click="changeSort('customer')"
+      >
+        <span class="arrow" :class="sort.type">
+          <v-icon class="icon">mdi-chevron-down</v-icon>
+        </span>
+        <p>Customer</p>
+      </div>
+      <div
+        class="head supplier"
+        :class="sort.name === 'supplier' ? 'active' : ''"
+        @click="changeSort('supplier')"
+      >
+        <span class="arrow" :class="sort.type">
+          <v-icon class="icon">mdi-chevron-down</v-icon>
+        </span>
+        <p>Supplier</p>
+      </div>
+      <div
+        class="head weight"
+        :class="sort.name === 'weight' ? 'active' : ''"
+        @click="changeSort('weight')"
+      >
+        <span class="arrow" :class="sort.type">
+          <v-icon class="icon">mdi-chevron-down</v-icon>
+        </span>
+        <p>Berat</p>
+      </div>
+      <div class="head action">
+        <p>Action</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -49,6 +131,7 @@ export default {
 
   data() {
     return {
+      type: 1,
       // insialisasi object
       messageSelected: null,
       bukaPopup: false,
@@ -187,6 +270,25 @@ export default {
           margin: 0;
         }
       }
+    }
+  }
+  .table-header {
+    position: relative;
+    width: 100%;
+    margin-top: 1rem;
+    height: 2rem;
+    background: $border-color;
+    padding: 0 2rem;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .head {
+      cursor: pointer;
+      position: relative;
+      height: 100%;
+      display: flex;
+      align-items: center;
     }
   }
 }
