aa04a5feee182c26d29184a53f31d82cfc43e7dc
Aditya930GO <60997538+Aditya930GO@users.noreply.github.com>
2021-06-28 18:52:41 +0700

    adit

--- server/index.js
+++ server/index.js
@@ -66,13 +66,13 @@ app
 
 
 // Error handlers
-app.use(function fourOhFourHandler (req, res) {
-  res.status(404).send()
-})
-app.use(function fiveHundredHandler (err, req, res, next) {
-  console.error(err)
-  res.status(500).send()
-})
+// app.use(function fourOhFourHandler (req, res) {
+//   res.status(404).send()
+// })
+// app.use(function fiveHundredHandler (err, req, res, next) {
+//   console.error(err)
+//   res.status(500).send()
+// })
 
 const server = http.createServer(app)
 const io = socketIO(server, {
